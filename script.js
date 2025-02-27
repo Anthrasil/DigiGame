6b92be747b3762ddf5bf87aadaf01e278a06f345
Anthrasil <raphaelchristopher.dorninger@gmail.com>
2025-02-27 17:23:05 +0100

    was wir gemacht haben Donerstag

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let test = "hello";
+const { link } = require("fs");
+
+let Aero = document.querySelector("#Aero");
+setInterval(() => {
+    let width = window.innerWidth / 2;
+    Aero.style.width = width + "px";
+    let height = window.innerHeight / 2;
+    Aero.style.height = height + "px";
+}, 1)
+Aero.addEventListener("click", () => {
+    alert("Moin Leute Trymax hier");
+});
